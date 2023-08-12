d3dd9a0e7b23556668b37d8a495e37f6c8b7bdff
Chris Trimble <chris.trimble3.ct@gmail.com>
2023-08-11 22:40:03 -0400

    Add Dropbear Support
    Added configuration for Dropbear to optionally disable root connection and passwords (to require keys).

--- Hardening-Dietpi.sh
+++ Hardening-Dietpi.sh
@@ -29,7 +29,7 @@ while true; do
                 echo "Installing..."
                 sudo apt install libpam-tmpdir needrestart debsecan debsums sed apt-utils -y
                 echo "Complete." 
-                break ;;
+                break;;
 
         [Nn]* ) echo 'Not Installing packages.' && break;;
 
@@ -46,7 +46,7 @@ while true; do
                 ##Install with DietPi-Software works well, so invoking DietPi-Software
                 sudo /boot/dietpi/dietpi-software install 73
                 echo "Complete." 
-                break ;;
+                break;;
 
         [Nn]* ) echo 'Not Installing Fail2Ban.' && break;;
 
@@ -73,7 +73,7 @@ while true; do
                 ##Enables auto update in /boot/dietpi.txt
                 sudo sed -i 's/CONFIG_CHECK_APT_UPDATES=.*/CONFIG_CHECK_APT_UPDATES=2/g' /boot/dietpi.txt
                 echo "Complete." 
-                break ;;
+                break;;
 
         [Nn]* ) echo "Not enabling automatic updates." && break;;
 
@@ -91,7 +91,7 @@ while true; do
                 echo 'install rds /bin/true' >> ~/disable-rds.conf 
                 sudo mv ~/disable-rds.conf /etc/modprobe.d/ 
                 echo "Complete."
-                break ;;
+                break;;
 
         [Nn]* ) echo "Leaving enabled." && break;;
 
@@ -107,7 +107,7 @@ while true; do
                 echo 'install sctp /bin/true' >> ~/disable-sctp.conf 
                 sudo mv ~/disable-sctp.conf /etc/modprobe.d/ 
                 echo "Complete." 
-                break ;;
+                break;;
 
         [Nn]* ) echo "Leaving enabled." && break;;
 
@@ -128,52 +128,91 @@ while true; do
                 echo "You will need SSH keys to fully secure SSH."
                 echo "Check out SSH-Key-Builder and then run this script again (https://github.com/Trimble-tech/SSH-Key-Builder)." 
                 exit ; 
-                break ;;
+                break;;
 
         * ) echo 'Yes or No?' ;;
     esac
 done
 
-echo "Hardening SSH service..."
-##Make backups of files before making changes
-##I iterate this verbally in script, since users may have issues with config/want peace of mind in headless (no display) setups.
-    mkdir ~/backups
-    touch ~/backups/sshd_config-OLD
-    touch ~/backups/dietpi.OLD
-    cp /etc/ssh/sshd_config ~/backups/sshd_config-OLD
-    cp /etc/ssh/sshd_config.d/dietpi.conf ~/backups/dietpi.OLD
-    echo "Original SSH file copied to sshd_config-OLD" ##Backups can't be in /etc/ssh because it makes apt/SSH errors
-    echo "Dietpi SSH file copied to dietpi.OLD"
-    echo ' '
-    echo "Contents of '~/backups':" && ls ~/backups
-    echo ' '
-    echo "To reverse changes copy these files over the originals."
-    echo "Original SSH file is /etc/ssh/sshd_config"
-    echo "Original DietPi file is /etc/ssh/sshd_config.d/dietpi.conf"
-    sleep 2
+while true; do
+    read -p "Is your server using OpenSSH or Dropbear? [O/D] " od
+        case $od in
+        [Oo]* )
 
-##SSHD
-    sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config ##Requires keys if uncommented
-    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config ##If you like root login change 'PermitRootLogin no' to 'PermitRootLogin prohibit-password'.
+                echo "Hardening SSH service..."
+                ##Make backups of files before making changes
+                ##I iterate this verbally in script, since users may have issues with config/want peace of mind in headless (no display) setups.
+                mkdir ~/backups
+                touch ~/backups/sshd_config-OLD
+                touch ~/backups/dietpi.OLD
+                cp /etc/ssh/sshd_config ~/backups/sshd_config-OLD
+                cp /etc/ssh/sshd_config.d/dietpi.conf ~/backups/dietpi.OLD
+                echo "Original SSH file copied to sshd_config-OLD" ##Backups can't be in /etc/ssh because it makes apt/SSH errors
+                echo "Dietpi SSH file copied to dietpi.OLD"
+                echo ' '
+                echo "Contents of '~/backups':" && ls ~/backups
+                echo ' '
+                echo "To reverse changes copy these files over the originals."
+                echo "Original SSH file is /etc/ssh/sshd_config"
+                echo "Original DietPi file is /etc/ssh/sshd_config.d/dietpi.conf"
+                ##Sleep for readability
+                sleep 2
 
-    sudo sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/g' /etc/ssh/sshd_config ##Limits login attempts in supplement to fail2ban (no ban but bounces without fail2ban)
-    sudo sed -i 's/#MaxSessions 10/MaxSessions 3/g' /etc/ssh/sshd_config ##Number of open SSH connections unlikely to ever be more than 3; 10 is excessive.
+        ##SSHD
+                sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config ##Requires keys if uncommented
+                sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config ##If you like root login change 'PermitRootLogin no' to 'PermitRootLogin prohibit-password'.
 
-    sudo sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding no/g' /etc/ssh/sshd_config ##Uncommonly needed
-    sudo sed -i 's/#TCPKeepAlive yes/TCPKeepAlive no/g' /etc/ssh/sshd_config ##Set with above option
+                sudo sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/g' /etc/ssh/sshd_config ##Limits login attempts in supplement to fail2ban (no ban but bounces without fail2ban)
+                sudo sed -i 's/#MaxSessions 10/MaxSessions 3/g' /etc/ssh/sshd_config ##Number of open SSH connections unlikely to ever be more than 3; 10 is excessive.
 
-    sudo sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 2/g' /etc/ssh/sshd_config ##Shortens timeout on connections
-    sudo sed -i 's/#Compression delayed/Compression no/g' /etc/ssh/sshd_config ##Connection compression has minor security issues but may be beneficial if your internet is dial-up (slow).
+                sudo sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding no/g' /etc/ssh/sshd_config ##Uncommonly needed
+                sudo sed -i 's/#TCPKeepAlive yes/TCPKeepAlive no/g' /etc/ssh/sshd_config ##Set with above option
 
-    sudo sed -i 's/X11Forwarding yes/X11Forwarding no/g' /etc/ssh/sshd_config ##Disable Graphical forwarding through SSH, only consider if remote desktops are in use.
+                sudo sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 2/g' /etc/ssh/sshd_config ##Shortens timeout on connections
+                sudo sed -i 's/#Compression delayed/Compression no/g' /etc/ssh/sshd_config ##Connection compression has minor security issues but may be beneficial if your internet is dial-up (slow).
 
-##Dietpi-conf (extra config file in Dietpi): Make sure your options here match SSHD options
-    sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config.d/dietpi.conf
-    sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config.d/dietpi.conf
+                sudo sed -i 's/X11Forwarding yes/X11Forwarding no/g' /etc/ssh/sshd_config ##Disable Graphical forwarding through SSH, only consider if remote desktops are in use.
 
-##Verifies integrity of ssh configuration to prevent breakage
-    sudo sshd -t -f /etc/ssh/sshd_config
-    sudo sshd -t -f /etc/ssh/sshd_config.d/dietpi.conf
+                ##Dietpi-conf (extra config file in Dietpi): Make sure your options here match SSHD options
+                sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config.d/dietpi.conf
+                sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config.d/dietpi.conf
+
+        ##Verifies integrity of ssh configuration to prevent breakage
+                sudo sshd -t -f /etc/ssh/sshd_config
+                sudo sshd -t -f /etc/ssh/sshd_config.d/dietpi.conf
+                
+                break;;
+
+        [Dd]* )
+                ##Dropbear has many different ways to be configured
+                    ##2FA and other options could be considered in future
+                    ##For now opting to require keys and disable root login to match OpenSSH
+
+                echo "Hardening Dropbear SSH service..."
+
+                mkdir ~/backups
+                touch ~/backups/dropbear.OLD
+                cp /etc/default/dropbear ~/backups/dropbear.OLD
+                echo "Original Dropbear SSH file copied to dropbear-OLD"
+                echo "Dietpi SSH file copied to dietpi.OLD"
+                echo ' '
+                echo "Contents of '~/backups':" && ls ~/backups
+                echo ' '
+                echo "To reverse changes copy these files over the originals."
+                echo "Original SSH file is /etc/default/dropbear"
+                ##Sleep for readability
+                sleep 2                
+
+                ##disable Passwords and Root SSH
+                ##I use ^ to specify only the line starting with DROPBEAR...
+                    ##Other lines would get modded otherwise
+                sudo sed -i 's/^DROPBEAR_EXTRA_ARGS=""/DROPBEAR_EXTRA_ARGS=" -g -s "/g' /etc/default/dropbear
+
+                ##sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+                break;;
+        * ) echo 'Yes or No?' ;;
+    esac
+done
 
 ##Mission critical systems may prefer only restarting some services like SSH instead of the whole system.
 echo "Some of these actions may require restarting services to take full effect."
@@ -184,18 +223,18 @@ while true; do
                 echo "Rebooting system, Goodbye!" 
                 sudo reboot 
                 exit ; 
-                break ;;
+                break;;
         
         [Dd]* ) 
                 echo "Entering Dietpi-Services..." 
                 sudo /boot/dietpi/dietpi-services
                 exit ; 
-                break ;;
+                break;;
 
         [Nn]* ) 
                 echo "Exiting, do not forget to reboot/restart services when possible." 
                 exit ; 
-                break ;;
+                break;;
 
         * ) echo "R will reboot, D will go to Dietpi-services, and N will do nothing."
     esac
